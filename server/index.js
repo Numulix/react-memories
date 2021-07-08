ea290b0ec9e02de7622c4da9f617b5c206a23387
Jovan Babic <jbabic999@gmail.com>
2021-07-08 19:51:23 +0200

    Styled form, todo: post request

--- server/index.js
+++ server/index.js
@@ -6,11 +6,11 @@ import postRoutes from './routes/posts.js'
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(express.json({ limit: "30mb", extended: true }))
 app.use(express.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors())
+
+app.use('/posts', postRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://memories_user:memoriesapp321@cluster0.am8ys.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const PORT = process.env.PORT || 5000;
