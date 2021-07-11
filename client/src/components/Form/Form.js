73000afa03609493f6b14b27576d52d20abd7340
Jovan Babic <jbabic999@gmail.com>
2021-07-11 12:52:42 +0200

    Created routes and actions for updating a post

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -2,11 +2,11 @@ import React, { useState } from 'react'
 import { TextField, Button, Typography, Paper } from '@material-ui/core'
 import FileBase from 'react-file-base64'
 import { useDispatch } from 'react-redux'
-import { createPost } from '../../actions/posts'
+import { createPost, updatePost } from '../../actions/posts'
 
 import useStyles from './styles'
 
-const Form = () => {
+const Form = ({ currentId, setCurrentId }) => {
   const [postData, setPostData] = useState({
     creator: '',
     title: '',
@@ -20,7 +20,11 @@ const Form = () => {
   const handleSubmit = (e) => {
     e.preventDefault()
 
-    dispatch(createPost(postData))
+    if (currentId) {
+      dispatch(updatePost(currentId, postData))
+    } else {
+      dispatch(createPost(postData))
+    }
   }
 
   const clear = () => {
