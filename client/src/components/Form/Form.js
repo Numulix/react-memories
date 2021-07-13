28e094256906ae364ee08ff8380b57af1471d539
Jovan Babic <jbabic999@gmail.com>
2021-07-13 15:03:42 +0200

    Functionalities for updating and deleting finished

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { TextField, Button, Typography, Paper } from '@material-ui/core'
 import FileBase from 'react-file-base64'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { createPost, updatePost } from '../../actions/posts'
 
 import useStyles from './styles'
@@ -14,8 +14,13 @@ const Form = ({ currentId, setCurrentId }) => {
     tags: '',
     selectedFile: '',
   })
+  const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null)
   const classes = useStyles()
   const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (post) setPostData(post)
+  }, [post])
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -25,16 +30,25 @@ const Form = ({ currentId, setCurrentId }) => {
     } else {
       dispatch(createPost(postData))
     }
+
+    clear()
   }
 
   const clear = () => {
-    
+    setCurrentId(null)
+    setPostData({
+      creator: '',
+      title: '',
+      message: '',
+      tags: '',
+      selectedFile: '',
+    })
   }
 
   return (
     <Paper className={classes.paper}>
       <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-        <Typography variant="h6">Creating a memory</Typography>
+        <Typography variant="h6">{currentId ? 'Editing' : 'Creating'} a memory</Typography>
         <TextField 
           name="creator" 
           variant="outlined" 
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,5 @@
 import PostMessage from '../models/postMessage.js'
+import mongoose from 'mongoose'
 
 export const getPosts = async (req, res) => {
   try {
@@ -27,7 +28,19 @@ export const updatePost = async (req, res) => {
 
   if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with given id')
 
-  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true })
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true })
 
   res.json(updatedPost)
+}
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params
+
+  if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with given id')
+
+  await PostMessage.findByIdAndRemove(id)
+
+  console.log('DELETE')
+
+  res.json({ message: 'Post deleted successfully' });
 }
